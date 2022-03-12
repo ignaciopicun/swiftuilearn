9ec827b1b3e026c9a7c05ab4b3801d9a46d51cdd
Nacho Picun <ignaciopicun@protonmail.com>
2022-03-12 17:30:58 -0300

    Finished 3 session: layout code and preview

--- swiftuilearn/ContentView.swift
+++ swiftuilearn/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26, height: 26)
+                .cornerRadius(20)
+            Text("Swift UI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours".uppercased())
+                .foregroundColor(.secondary)
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text(
+                "Build an iOS app for iOS 15 with custom layouts, animations and ..."
+            )
+                .foregroundColor(.secondary)
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(.all, 20)
+        .frame(height: 350)
+        .background(Color("Background"))
+        .cornerRadius(30)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
     }
 }
 
