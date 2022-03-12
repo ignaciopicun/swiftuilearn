f87219165b68c15a0bb0d814ebbd5ffe0445d31e
Nacho Picun <ignaciopicun@protonmail.com>
2022-03-12 16:00:58 -0300

    Initial Commit

---
+++ swiftuilearn/swiftuilearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuilearnApp.swift
+//  swiftuilearn
+//
+//  Created by Nacho on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftuilearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
